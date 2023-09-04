ad7ba494ac9c26da8a7ec1f55a4254656e2f7a44
swarnadeep <aritrasarkar2002@gmail.com>
2023-09-04 20:04:23 +0530

    starter

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+require("dotenv").config();
+
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins = config.plugins || [];
+
+    config.plugins = [
+      ...config.plugins,
+
+      new Dotenv({
+        path: path.join(__dirname, ".env"),
+        systemvars: true,
+      }),
+    ];
+
+    return config;
+  },
+};
---
+++ src/app/page.jsx
@@ -0,0 +1,47 @@
+"use client"
+
+import {db} from "../db/firebase-config"
+import { getDocs, collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+
+export default function Home() {
+
+  const [blogs, setBlogs] = useState([]);
+
+  const blogsCollectionRef = collection(db, "blogs");
+
+  useEffect(() => {
+    const getblogs = async () => {
+      try {
+        const data = await getDocs(blogsCollectionRef);
+
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        setBlogs(filteredData)
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getblogs();
+  }, []);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+      {/* <div className="">
+        {blogs.map((data) => (
+          <div className="grid grid-rows-2 p-5 m-5 bg-teal-900" key={data.id}>
+            <h1>{data.title}</h1>
+            <p>{data.slug}</p>
+            <p>{data.desc}</p>
+          </div>
+        ))}
+      </div> */}
+    </main>
+  )
+}
